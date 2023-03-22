dd2f6e020db5e091f2f2ed059bd5e9f99c5e1b72
jessyca <jessyca@DESKTOP-LHFVUB6>
2023-03-22 16:12:24 +0100

    MENSAJE PAQUETE RAMA1

---
+++ Proyecto_1_/src/PaqueteRAMA1/RAMA1.java
@@ -0,0 +1,12 @@
+package PaqueteRAMA1;
+
+public class RAMA1 {
+
+	public static void main(String[] args) {
+		
+		// JESSYCA ARAUJO OLIVEIRA
+
+		System.out.println("Mensaje por pantalla paquete RAMA1");
+	}
+
+}
